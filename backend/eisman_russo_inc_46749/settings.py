98b9fed73fca664ad4d02d34355dd6112d55affa
kamran4322 <muhammad.kamran@crowdbotics.com>
2024-07-26 19:13:54 +0500

    cors allow

--- backend/eisman_russo_inc_46749/settings.py
+++ backend/eisman_russo_inc_46749/settings.py
@@ -60,6 +60,8 @@ SITE_ID = 1
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = env.bool("SECURE_REDIRECT", default=False)
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -70,7 +72,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites'
+    'django.contrib.sites',
+    'corsheaders',
 ]
 LOCAL_APPS = [
     'home',
@@ -98,6 +101,7 @@ MODULES_APPS = get_modules()
 INSTALLED_APPS += LOCAL_APPS + THIRD_PARTY_APPS + MODULES_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
