6a7bc717b85fff19a4ab2deba59836767ab09a9a
kamran4322 <muhammad.kamran@crowdbotics.com>
2024-08-06 12:55:07 +0500

    allow admin to un link device

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -261,6 +261,7 @@ class UserUpdateSerializer(serializers.ModelSerializer):
             "name",
             "phone_number",
             "address",
+            "device_id",
             "latitude",
             "longitude",
             "role",
